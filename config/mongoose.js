69449b6d897c585342b868ce252765090cfe2a1e
James-Mac <james@aehr.org>
2014-09-23 09:12:34 -0600

    Refactor the mongoose connection config module

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,8 +5,13 @@
 var mongoose = require('mongoose'),
     winston  = require('winston');
 
+var connection;
 
 exports.init = function () {
-  winston.info('Connecting to mongodb...');
-  return mongoose.connect('localhost', 'trust');
+  if( !connection ) {
+    winston.info('Connecting to mongodb...');
+    connection = mongoose.connect('localhost', 'trust');
+  }
+
+  return connection;
 };
