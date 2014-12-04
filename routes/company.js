9233ae0b9ad4bf7fbfa99e94a7cc3716e3638529
James-Mac <james@aehr.org>
2014-12-04 08:07:51 -0700

    Activate permissions on company routes

--- routes/company.js
+++ routes/company.js
@@ -1,8 +1,14 @@
 var express        = require('express'),
     companyHandler = require('../handlers/company');
 
+var sessionMiddleware       = require('../lib/security/middleware/session'),
+    authorizationMiddleware = require('../lib/security/middleware/authorization');
+
 module.exports = function (app) {
   var companyRouter = express.Router();
+
+  companyRouter.use( sessionMiddleware('Session') );
+  companyRouter.use( authorizationMiddleware({ allow: 'admin' }) );
 
   companyRouter.get('/', companyHandler.fetchAll);
   companyRouter.get('/:id', companyHandler.fetchByID);
