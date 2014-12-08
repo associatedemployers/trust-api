8274e44b2776fcad72b8321f83855b513b0c909b
James-Mac <james@aehr.org>
2014-12-08 15:46:31 -0700

    Fixes #15

--- models/location.js
+++ models/location.js
@@ -12,17 +12,16 @@ var createModel = require('./helpers/create-model');
 var locationSchema = new Schema({
   ebmsNumber:          String,
   legacyCompanyNumber: String,
-  legacyCompanyName:   String, // Don't use this
   soleProprietorship:  Boolean,
   embeddedDeductible:  Boolean,
   legacyInactive:      Boolean,
 
   address: {
-   line1:   String,
-   line2:   String,
-   city:    String,
-   state:   String,
-   zipcode: String,
+    line1:   String,
+    line2:   String,
+    city:    String,
+    state:   String,
+    zipcode: String,
   },
   
   contact: {
@@ -34,7 +33,7 @@ var locationSchema = new Schema({
   employees: [{ type: mongoose.Schema.ObjectId, ref: 'Employee' }],
 
   legacyEffectiveDate: Date,
-  time_stamp:          String,
+  time_stamp:          Date,
 });
 
 module.exports = createModel('Location', locationSchema);
