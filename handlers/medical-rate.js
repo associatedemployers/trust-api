16a6e5d8bfb0f31322e5a74668da8cf85fba559f
James-Mac <james@aehr.org>
2014-12-09 14:36:30 -0700

    Mixin use

--- handlers/medical-rate.js
+++ handlers/medical-rate.js
@@ -3,80 +3,11 @@ var winston   = require('winston').loggers.get('default'),
     normalize = require('../config/data-normalization'),
     respond   = require('./response');
 
-var MedicalRate = require('../models/medical-rate');
+var MedicalRate   = require('../models/medical-rate'),
+    ResourceMixin = require('../lib/mixins/resource-handler');
 
-exports.fetchAll = function ( req, res, next ) {
-  var query  = req.query,
-      limit  = parseFloat(query.limit) || null,
-      page   = parseFloat(query.page)  || 0,
-      select = query.select || '',
-      skip   = page * limit,
-      sort   = query.sort || { time_stamp: 1 };
-
-  if( req.query.ids ) {
-    query._id = {
-      $in: req.query.ids
-    };
-
-    delete query.ids;
-  }
-
-  delete query.limit;
-  delete query.page;
-  delete query.sort;
-  delete query.select;
-
-  for ( var key in query ) {
-    var v = query[ key ];
-    if( v === 'exists' ) {
-      query[ key ] = {
-        $exists: true
-      };
-    } else if ( v === 'nexists' ) {
-      query[ key ] = {
-        $exists: false
-      };
-    }
-  }
-
-  console.log(query, limit, page, skip);
-
-  MedicalRate
-  .find( query )
-  .sort( sort )
-  .skip( Math.abs( skip ) )
-  .limit( Math.abs( limit ) )
-  .select( select )
-  .exec(function ( err, records ) {
-    if( err ) {
-      return respond.error.res( res, err, true );
-    }
-
-    MedicalRate.count( query, function ( err, count ) {
-      if( err ) {
-        return respond.error.res( res, err, true );
-      }
-
-      res.json( normalize.medicalRate( records, { totalRecords: count } ) );
-    });
-  });
-};
-
-exports.fetchByID = function ( req, res, next ) {
-  var id = req.params.id;
-
-  if( !id ) {
-    return respond.error.res( res, 'Please specify an id in the url.' );
-  }
-
-  MedicalRate.findById(id, function ( err, record ) {
-    if( err ) {
-      return respond.error.res( res, err, true );
-    }
-
-    res.json( normalize.medicalRate( record ) );
-  });
-};
+exports.fetchAll = ResourceMixin.getAll('MedicalRate');
+exports.fetchByID = ResourceMixin.getById('MedicalRate');
 
 exports.create = function ( req, res, next ) {
   respond.code.notimplemented(res);
--- handlers/user.js
+++ handlers/user.js
@@ -4,88 +4,12 @@ var winston   = require('winston').loggers.get('default'),
     respond   = require('./response'),
     _         = require('lodash');
 
-var User     = require('../models/user'),
-    ObjectId = require('mongoose').Types.ObjectId;
+var User          = require('../models/user'),
+    ObjectId      = require('mongoose').Types.ObjectId,
+    ResourceMixin = require('../lib/mixins/resource-handler');
 
-exports.fetchAll = function ( req, res, next ) {
-  console.log(req.query);
-  var query  = req.query,
-      limit  = parseFloat(query.limit) || null,
-      page   = parseFloat(query.page)  || 0,
-      select = query.select || '',
-      skip   = page * limit,
-      sort   = query.sort || { time_stamp: 1 };
-
-  if( req.query.ids ) {
-    query._id = {
-      $in: req.query.ids
-    };
-
-    delete query.ids;
-  }
-
-  delete query.limit;
-  delete query.page;
-  delete query.sort;
-  delete query.select;
-
-  for ( var key in query ) {
-    var v = query[ key ];
-    if( v === 'exists' ) {
-      query[ key ] = {
-        $exists: true
-      };
-    } else if ( v === 'nexists' ) {
-      query[ key ] = {
-        $exists: false
-      };
-    }
-  }
-
-  console.log(query, limit, page, skip);
-
-  User
-  .find( query )
-  .sort( sort )
-  .skip( Math.abs( skip ) )
-  .limit( Math.abs( limit ) )
-  .select( select )
-  .exec(function ( err, records ) {
-    if( err ) {
-      return respond.error.res( res, err, true );
-    }
-
-    User.count( query, function ( err, count ) {
-      if( err ) {
-        return respond.error.res( res, err, true );
-      }
-
-      res.json( normalize.user( records, { totalRecords: count } ) );
-    });
-  });
-};
-
-exports.fetchByID = function ( req, res, next ) {
-  var id = req.params.id;
-
-  if( !id ) {
-    return respond.error.res( res, 'Please specify an id in the url.' );
-  }
-
-  User
-  .findById(id)
-  .exec(function ( err, record ) {
-    if( err ) {
-      return respond.error.res( res, err, true );
-    }
-
-    if( !record ) {
-      return respond.code.notfound( res );
-    }
-
-    res.json( normalize.user( record ) );
-  });
-};
+exports.fetchAll = ResourceMixin.getAll('User');
+exports.fetchByID = ResourceMixin.getById('User');
 
 exports.create = function ( req, res, next ) {
   var payload = req.body.user;
