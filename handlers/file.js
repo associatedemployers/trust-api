84b9b139ad796e612b431e534343ce6ece065792
James-Mac <james@aehr.org>
2014-12-10 14:28:51 -0700

    Use resourceMixin on file handler

--- handlers/file.js
+++ handlers/file.js
@@ -4,7 +4,8 @@ var winston   = require('winston').loggers.get('default'),
     respond   = require('./response'),
     fs        = require('fs-extra');
 
-var File = require('../models/file');
+var File = require('../models/file'),
+    ResourceMixin = require('../lib/mixins/resource-handler');
 
 exports.get = function ( req, res, next ) {
   if( !req.params.path ) {
@@ -45,72 +46,9 @@ exports.get = function ( req, res, next ) {
   });
 };
 
-exports.fetchAll = function ( req, res, next ) {
-  var query  = req.query,
-      limit  = parseFloat(query.limit) || null,
-      page   = parseFloat(query.page)  || 0,
-      select = query.select || '',
-      skip   = page * limit,
-      sort   = query.sort || { time_stamp: 1 };
 
-  if( req.query.ids ) {
-    query._id = {
-      $in: req.query.ids
-    };
-
-    delete query.ids;
-  }
-
-  delete query.limit;
-  delete query.page;
-  delete query.sort;
-  delete query.select;
-
-  for ( var key in query ) {
-    var v = query[ key ];
-    if( v === 'exists' ) {
-      query[ key ] = {
-        $exists: true
-      };
-    } else if ( v === 'nexists' ) {
-      query[ key ] = {
-        $exists: false
-      };
-    }
-  }
-
-  console.log(query, limit, page, skip);
-
-  File
-  .find( query )
-  .sort( sort )
-  .skip( Math.abs( skip ) )
-  .limit( Math.abs( limit ) )
-  .select( select )
-  .exec(function ( err, records ) {
-    if( err ) {
-      return respond.error.res( res, err, true );
-    }
-
-    res.json( normalize.file( records ) );
-  });
-};
-
-exports.fetchByID = function ( req, res, next ) {
-  var id = req.params.id;
-
-  if( !id ) {
-    return respond.error.res( res, 'Please specify an id in the url.' );
-  }
-
-  File.findById(id, function ( err, record ) {
-    if( err ) {
-      return respond.error.res( res, err, true );
-    }
-
-    res.json( normalize.file( record ) );
-  });
-};
+exports.fetchAll = ResourceMixin.getAll('File');
+exports.fetchByID = ResourceMixin.getById('File');
 
 exports.create = function ( req, res, next ) {
   respond.code.notimplemented(res);
